3d4a73b540a2b6f2260fc104e79e9fbaf16c0931
Ali AKCA <ali@akca.io>
2023-05-26 18:28:39 +0200

    chore: install using go

--- pkg/gale/ghx.go
+++ pkg/gale/ghx.go
@@ -2,17 +2,12 @@ package gale
 
 import "dagger.io/dagger"
 
-// TODO: use releases instead of main branch. This is temporary hack.
+// TODO: use releases instead of latest. This is temporary hack.
 
 func buildExecutorFromSource(client *dagger.Client) *dagger.File {
-	dir := client.Git("github.com/aweris/ghx").Branch("main").Tree()
-
 	return client.Container().
 		From("golang:1.20-bullseye").
-		WithUnixSocket("/var/run/docker.sock", client.Host().UnixSocket("/var/run/docker.sock")).
-		WithDirectory("/src", dir).
-		WithWorkdir("/src").
-		WithExec([]string{"go", "build", "-o", "/src/build/ghx", "."}).
-		Directory("/src/build").
+		WithExec([]string{"go", "install", "github.com/aweris/ghx@latest"}).
+		Directory("/go/bin").
 		File("ghx")
 }
