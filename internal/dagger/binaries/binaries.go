7d2cebe3221c63ed3e1dd0c3a6738f60ecb04749
Ali AKCA <ali@akca.io>
2023-06-21 15:13:53 +0200

    feat: use go install for non semver ghx binaries

--- internal/dagger/binaries/binaries.go
+++ internal/dagger/binaries/binaries.go
@@ -5,15 +5,32 @@ import (
 	"fmt"
 
 	"dagger.io/dagger"
+
+	"github.com/Masterminds/semver/v3"
+
+	"github.com/aweris/gale/internal/dagger/images"
 )
 
 // Ghx returns a dagger file for the ghx binary. It'll return an error if the binary is not available.
 func Ghx(ctx context.Context, client *dagger.Client, version string) (*dagger.File, error) {
-	file := client.Container().
-		Pipeline("Binaries").Pipeline("ghx").
-		From(fmt.Sprintf("ghcr.io/aweris/ghx:%s", version)).
-		Directory("/usr/local/bin").
-		File("ghx")
+	var file *dagger.File
+
+	// if the version is not a valid semver, we'll assume that it's a branch name or commit hash,
+	// and we'll try to install using go
+	_, err := semver.NewVersion(version)
+	if err != nil {
+		file = images.GoBase(client).
+			Pipeline("Install ghx binary").
+			WithExec([]string{"go", "install", fmt.Sprintf("github.com/aweris/ghx@%s", version)}).
+			Directory("/go/bin").
+			File("ghx")
+	} else {
+		file = client.Container().
+			Pipeline("Download ghx binary").
+			From(fmt.Sprintf("ghcr.io/aweris/ghx:%s", version)).
+			Directory("/usr/local/bin").
+			File("ghx")
+	}
 
 	// check, if the file doesn't exist or is empty
 	if size, err := file.Size(ctx); size == 0 || err != nil {
--- internal/dagger/images/images.go
+++ internal/dagger/images/images.go
@@ -1,10 +1,22 @@
 package images
 
-import "dagger.io/dagger"
+import (
+	"runtime"
+	"strings"
+
+	"dagger.io/dagger"
+)
 
 // RunnerBase returns a container with the base image for the runner.
 func RunnerBase(client *dagger.Client) *dagger.Container {
 	return client.Container().
-		Pipeline("Images").Pipeline("Runner Base").
+		Pipeline("Runner Base Image").
 		From("ghcr.io/catthehacker/ubuntu:act-22.04")
+}
+
+// GoBase returns a container with the base image for the go
+func GoBase(client *dagger.Client) *dagger.Container {
+	return client.Container().
+		Pipeline("Go Base Image").
+		From("golang:" + strings.TrimPrefix(runtime.Version(), "go"))
 }
