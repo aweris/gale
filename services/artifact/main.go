241aa7b81fe49bf9b3b4a5fd70c83bc7a242d981
Ali AKCA <ali@akca.io>
2023-06-26 16:19:20 +0200

    feat: make artifact service configurable

--- services/artifact/main.go
+++ services/artifact/main.go
@@ -1,11 +1,43 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/pflag"
+)
 
 func main() {
-	srv := NewLocalService(os.TempDir())
+	var (
+		artifactDir string
+		port        string
+	)
 
-	if err := Serve("8080", srv); err != nil {
-		panic(err)
+	pflag.StringVar(&artifactDir, "artifact-dir", "/artifacts", "Directory to store artifacts")
+	pflag.StringVar(&port, "port", "8080", "Port to artifact service will listen on")
+
+	bindEnv(pflag.Lookup("artifact-dir"), "ARTIFACT_DIR")
+	bindEnv(pflag.Lookup("port"), "PORT")
+
+	pflag.Parse()
+
+	if err := Serve(port, NewLocalService(artifactDir)); err != nil {
+		fmt.Printf("Error starting artifact service: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func bindEnv(fn *pflag.Flag, env string) {
+	if fn == nil || fn.Changed {
+		return
+	}
+
+	val := os.Getenv(env)
+
+	if len(val) > 0 {
+		if err := fn.Value.Set(val); err != nil {
+			log.Fatalf("failed to bind env: %v\n", err)
+		}
 	}
 }
