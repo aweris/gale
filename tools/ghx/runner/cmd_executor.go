89f38bc0f565b6880bb48fd5b070e41108153a83
Ali AKCA <ali@akca.io>
2023-07-28 02:30:15 +0200

    add parse workflow commands from cmd output

--- tools/ghx/runner/cmd_executor.go
+++ tools/ghx/runner/cmd_executor.go
@@ -1,19 +1,24 @@
 package runner
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
 
+	"github.com/aweris/gale/internal/core"
 	"github.com/aweris/gale/tools/ghx/actions"
 )
 
 type CmdExecutor struct {
-	args []string             // args to pass to the command
-	env  map[string]string    // env to pass to the command as environment variables
-	ec   *actions.ExprContext // ec is the expression context to evaluate the github expressions
+	args     []string                // args to pass to the command
+	env      map[string]string       // env to pass to the command as environment variables
+	ec       *actions.ExprContext    // ec is the expression context to evaluate the github expressions
+	commands []*core.WorkflowCommand // commands is the list of commands that are executed in the step
 }
 
 func NewCmdExecutorFromStepAction(sa *StepAction, entrypoint string) *CmdExecutor {
@@ -39,7 +44,7 @@ func NewCmdExecutorFromStepAction(sa *StepAction, entrypoint string) *CmdExecuto
 	return &CmdExecutor{
 		args: []string{"node", fmt.Sprintf("%s/%s", sa.Action.Path, entrypoint)},
 		env:  env,
-		ec:   &actions.ExprContext{}, // empty expression context TODO: provide the actual context
+		ec:   actions.NewExprContext(),
 	}
 }
 
@@ -52,15 +57,18 @@ func NewCmdExecutorFromStepRun(sr *StepRun) *CmdExecutor {
 	return &CmdExecutor{
 		args: args,
 		env:  make(map[string]string),
-		ec:   &actions.ExprContext{}, // empty expression context TODO: provide the actual context
+		ec:   actions.NewExprContext(),
 	}
 }
 
 func (c *CmdExecutor) Execute(_ context.Context) error {
 	cmd := exec.Command(c.args[0], c.args[1:]...)
 
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	stdout := bytes.NewBuffer(nil)
+	stderr := bytes.NewBuffer(nil)
+	rawout := bytes.NewBuffer(nil)
+
+	cmd.Stderr = io.MultiWriter(stderr, os.Stderr)
 
 	// add environment variables
 
@@ -81,5 +89,42 @@ func (c *CmdExecutor) Execute(_ context.Context) error {
 
 	cmd.Env = env
 
-	return cmd.Run()
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		scanner := bufio.NewScanner(stdoutPipe)
+		for scanner.Scan() {
+			output := scanner.Text()
+
+			// write to stdout as it is so we can keep original formatting
+			rawout.WriteString(output)
+			rawout.WriteString("\n") // scanner strips newlines
+
+			isCommand, command := core.ParseCommand(output)
+
+			// print the output if it is a regular output
+			if !isCommand {
+				log.Info(output)
+
+				// write to stdout as it is so we can keep original formatting
+				stdout.WriteString(output)
+				stdout.WriteString("\n") // scanner strips newlines
+
+				continue
+			}
+
+			// add the command to the list of commands to keep it as artifact
+			c.commands = append(c.commands, command)
+		}
+	}()
+
+	return cmd.Wait()
 }
