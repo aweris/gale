3205f8a99dec7c3061fec7dd1b8d3a3d0e525842
Ali AKCA <ali@akca.io>
2023-08-29 10:49:49 +0200

    chore: update demo (#105)

--- demo/const.go
+++ demo/const.go
@@ -3,5 +3,5 @@ package main
 const (
 	BinDir        = "./bin"
 	DaggerVersion = "0.8.4"
-	GaleVersion   = "v0.0.5"
+	GaleVersion   = "v0.0.6"
 )
--- demo/main.go
+++ demo/main.go
@@ -12,6 +12,7 @@ func main() {
 	d.Add(list(), "list", "List all workflows and jobs under it for current repositories `main` branch")
 	d.Add(run(), "run", "Run golangci-lint job from ci/workflows/lint workflow for aweris/gale repository default branch")
 	d.Add(lintGoreleaser(), "lint-goreleaser", "Run golangci job from golangci-lint workflow for goreleaser/goreleaser repository tag v1.19.2")
+	d.Add(shellRun(), "shell-run", "Run example workflow contains steps with different shells")
 	d.Add(testDagger(), "test-dagger", "Run sdk-go job from test workflow for dagger/dagger repository branch main")
 	d.Add(testCache(), "test-cache", "Use cache in the workflow")
 
@@ -45,6 +46,16 @@ func lintGoreleaser() *demo.Run {
 	r.Step([]string{"Contents of the workflow file"}, demo.S("curl https://raw.githubusercontent.com/goreleaser/goreleaser/v1.19.2/.github/workflows/lint.yml"))
 
 	r.Step([]string{"Run the workflow from custom directory for goreleaser/goreleaser repository"}, env.RunGaleWithDagger("run --repo goreleaser/goreleaser --tag v1.19.2 golangci-lint golangci"))
+
+	return r
+}
+
+func shellRun() *demo.Run {
+	r := demo.NewRun("Run example workflow contains steps with different shells")
+
+	r.Step([]string{"Contents of the workflow file"}, demo.S("curl https://raw.githubusercontent.com/aweris/gale/main/ci/workflows/step-run.yaml"))
+
+	r.Step([]string{"Run the workflow from custom directory for current repository"}, env.RunGaleWithDagger("run --repo aweris/gale --workflows-dir ci/workflows shell run"))
 
 	return r
 }
